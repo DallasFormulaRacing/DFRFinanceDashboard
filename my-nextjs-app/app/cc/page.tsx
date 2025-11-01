458e74fef89ba6418ab34dcdc6386434b760c5bf
Haniyyah <hamidhaniyyahb@gmail.com>
2025-11-01 18:33:38 -0500

    sidebar.tsx + shadcn components integrated

---
+++ my-nextjs-app/app/cc/page.tsx
@@ -0,0 +1,9 @@
+// app/po/internal-combustion/page.tsx
+
+export default function CostCenterPage() {
+  return (
+    <div>
+        <p className="text-white">This will show the Cost Center information.</p>
+    </div>
+  );
+}
