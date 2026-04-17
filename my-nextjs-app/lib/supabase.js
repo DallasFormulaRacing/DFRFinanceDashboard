7c1a65956e21801222553e43a673eb340928285f
Farah Khalil Ahamed <farah.fathima04@gmail.com>
2026-04-17 15:48:57 -0500

    added env variables + fixed dependencies

--- my-nextjs-app/lib/supabase.js
+++ my-nextjs-app/lib/supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://htvjkpsdpwwvxrkcjjdz.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh0dmprcHNkcHd3dnhya2NqamR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2NDM4NDQsImV4cCI6MjA4ODIxOTg0NH0.kHz_Kp3Z_qqxyKcs_eXsh7s62CAqJrp1T8e7Dmj2g9w'
-
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
